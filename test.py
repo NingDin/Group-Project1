21390628c745c7f25c69864bba1f1babcfe82068
NingDin <114199756+NingDin@users.noreply.github.com>
2022-12-08 18:55:54 -0500

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline
-sentiment_pipeline = pipeline("sentiment-analysis")
-data = ["I love you","I am okay", "I hate you","Wow: pending home sales now down by nearly 37% year/year, which is worst annual decline on record (going back to 2002)" ]
-print(sentiment_pipeline(data))
